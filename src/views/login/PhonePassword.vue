955fd7dd3c9c35c01bf3a77c02a9b819903c8738
horanhe <horanhe@tencent.com>
2024-03-25 13:33:01 +0800

    feat: add bootstrap form style

--- src/views/login/PhonePassword.vue
+++ src/views/login/PhonePassword.vue
@@ -20,7 +20,8 @@ const {errors, defineField, handleSubmit, isSubmitting} = useForm<Form>({
 const [phone, phoneAttrs] = defineField('phone')
 const [password, passwordAttrs] = defineField('password')
 
-const {meta, handleBlur} = useField<number>('phone')
+const phoneField = useField<number>('phone')
+const passwordField = useField<string>('password')
 
 function onSuccess(values: Form) {
     console.log("success", values.phone)
@@ -32,23 +33,34 @@ function onSuccess(values: Form) {
   });
 }
 
-function onInvalidSubmit({ values, errors, results }) {
-    console.log("onInvalidSubmit", { values, errors, results })
-}
-
-const onSubmit = handleSubmit(onSuccess, onInvalidSubmit)
+const onSubmit = handleSubmit(onSuccess)
 
 </script>
 
 <template>
-    <form autocomplete="on" @submit.prevent="onSubmit" :validation-schema="schema">
-        <label for="phone">手机号</label>
-        <input name="phone" v-model="phone" v-bind="phoneAttrs" id="phone" @blur="handleBlur"/>
-        <span v-if="!meta.valid && meta.touched">{{ errors.phone }}</span>
-        <label for="password">密码</label>
-        <input name="password" v-model="password" v-bind="passwordAttrs" type="password" id="password" />
+    <form autocomplete="on" @submit.prevent="onSubmit" novalidate>
+        <div class="row mb-3">
+            <label for="phone" class="col-sm-2 col-form-label">手机号</label>
+            <div class="col-sm-10">
+                <input name="phone" class="form-control" v-model="phone" v-bind="phoneAttrs" id="phone" @blur="phoneField.handleBlur"/>
+                <div v-if="!phoneField.meta.valid && phoneField.meta.touched" class="invalid-feedback">{{ phoneField.errorMessage }}</div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="password" class="col-sm-2 col-form-label">密码</label>
+            <div class="col-sm-8">
+                <input name="password" class="form-control" 
+                :class="{ 'is-valid': passwordField.meta.valid, 'is-invalid': passwordField.meta.touched && !passwordField.meta.valid }"
+                v-model="password"
+                v-bind="passwordAttrs"
+                type="password"
+                id="password" 
+                @blur="passwordField.handleBlur"
+                aria-describedby="validationPasswordFeedback"
+                />
+            </div>
+            <div class="col-sm-2 invalid-feedback" id="validationPasswordFeedback" v-if="!passwordField.meta.valid && passwordField.meta.touched">{{ passwordField.errorMessage }}</div>
+        </div>
         <button :disabled="isSubmitting" type="submit">{{ isSubmitting ? "请求中..." : "登录" }}</button>
     </form>
-    <pre>errors: {{ errors }}</pre>
-    <pre>meta: {{ meta }}</pre>
 </template>
